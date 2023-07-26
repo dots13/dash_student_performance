6d0e6b70d3d2dbdbf6a8ae996cae482329c70319
dots_13 <stasya.dots@gmail.com>
2023-07-26 17:48:47 +0200

    add displot

--- src/app.py
+++ src/app.py
@@ -12,7 +12,6 @@ import pandas as pd
 import numpy as np
 import plotly.graph_objects as go
 import plotly.figure_factory as ff
-import seaborn as sns
 
 jupyter_dash.default_mode="external"
 
@@ -461,6 +460,17 @@ def generate_chart(feature, sep_feature):
         fig = create_hist(feature, sep_feature, fig)
     else:
         fig.add_trace(go.Histogram(x=df[feature], marker_color=color_palette[0], histnorm='probability'))
+        fig_dis = ff.create_distplot([df[feature]], [feature])
+
+        normal_x = fig_dis.data[1]['x']
+        normal_y = fig_dis.data[1]['y']
+
+        fig.add_trace(go.Scatter(x=normal_x, y=normal_y, mode='lines',
+                                 line=dict(color='rgba(0,255,0, 1)',
+                                            # dash = 'dash'
+                                            width=1),
+                                 name='normal'
+                                 ))
 
     fig.update_traces(opacity=0.4)
     fig.update_layout(
